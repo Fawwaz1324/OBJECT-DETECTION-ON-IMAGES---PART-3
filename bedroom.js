c53367212ae23c489d7fda877fce4b90b412b83a
Fawwaz1324 <124411333+Fawwaz1324@users.noreply.github.com>
2024-03-10 09:52:24 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,55 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('Bedroom.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML  = "Status :Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+    if(status !="")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#03421c");
+            percent = floor(objects[i].confidence * 100);
+            stroke("#03421c");
+            textSize(15);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y-170);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x+30, objects[i].y-170, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
